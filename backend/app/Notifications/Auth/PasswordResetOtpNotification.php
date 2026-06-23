7b966f18cdc4b117c745eda4e359ffcaa97c3481
ADHI prasetyo wibowo <adhiprasetyow07@gmail.com>
2026-06-24 00:10:05 +0700

    perf: queue email notifications to fix slow login and registration

--- backend/app/Notifications/Auth/PasswordResetOtpNotification.php
+++ backend/app/Notifications/Auth/PasswordResetOtpNotification.php
@@ -3,10 +3,11 @@
 namespace App\Notifications\Auth;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class PasswordResetOtpNotification extends Notification
+class PasswordResetOtpNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
